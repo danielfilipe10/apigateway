b3647c0dcded2ab9aa5fa32780381e8dd8b5209d
Daniel Batista <danielfilipebatista@gmail.com>
2020-03-17 12:01:37 +0000

    Ocelot first configuration

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace ApiGateway
 {
@@ -13,14 +16,65 @@ namespace ApiGateway
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext,config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json",true,true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true)
+                        .AddJsonFile("ocelot.json")
+                        .AddEnvironmentVariables();
+
+                })
+                .ConfigureServices(s => 
+                {
+                    s.AddOcelot();
+                })
+                .ConfigureLogging((hostingContext,logging) => 
+                {
+                    //add your logging
+                })
+                .UseIISIntegration()
+                .Configure(app => 
+                {
+                    app.UseOcelot().Wait();
+                })
+                .Build()
+                .Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        // public static IHostBuilder CreateHostBuilder(string[] args) =>
+            
+        //     new WebHostBuilder()
+        //         .UseKestrel()
+        //         .UseContentRoot(Directory.GetCurrentDirectory())
+        //         .ConfigureAppConfiguration((hostingContext,config) =>
+        //         {
+        //             config
+        //                 .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        //                 .AddJsonFile("appsettings.json",true,true)
+        //                 .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true)
+        //                 .AddJsonFile("ocelot.json")
+        //                 .AddEnvironmentVariables();
+
+        //         })
+        //         .ConfigureServices(s => 
+        //         {
+        //             s.AddOcelot();
+        //         })
+        //         .ConfigureLogging((hostingContext,logging) => 
+        //         {
+        //             //add your logging
+        //         })
+        //         .UseIISIntegration()
+        //         .Configure(app => 
+        //         {
+        //             app.UseOcelot().Wait();
+        //         })
+        //         .Build()
+        //         .Run(); 
     }
 }
