29428231c0f19cb0510aeff2d5444185d0972493
Daniel Batista <danielfilipebatista@gmail.com>
2020-03-18 19:12:06 +0000

    Routing working. Some clean-up

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace ApiGateway
 {
@@ -25,7 +27,7 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +41,8 @@ namespace ApiGateway
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseOcelot().Wait();
 
             app.UseAuthorization();
 
