ac08c6752552156a5d56147b249b39b458117b24
Daniel Batista <danielfilipebatista@gmail.com>
2020-03-30 19:25:04 +0100

    Added connection to postgres. Identity. (still code soup)

---
+++ AuthServer/Data/AuthDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data
+{
+    public class AuthDbContext : IdentityDbContext<User>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.HasDefaultSchema("public");
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- AuthServer/Startup.cs
+++ AuthServer/Startup.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthServer.Data;
 using AuthServer.Services;
 using AuthServer.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -31,6 +34,22 @@ namespace AuthServer
             services.AddOptions();
             services.Configure<AuthService.Audience>(Configuration.GetSection("Audience"));
 
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("AuthDbContext"));
+            });
+
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<AuthDbContext>()
+            .AddDefaultTokenProviders();
+
 
             services.AddScoped<IAuthService, AuthService>();
         }
@@ -42,6 +61,7 @@ namespace AuthServer
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseAuthentication();
             app.UseMvc();
             //app.UseRouting();
             //app.UseEndpoints(endpoints =>
